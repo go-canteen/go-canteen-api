47cad7ad694855a9513ce83f74880c22f1fcf5d3
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2022-09-17 23:27:09 +0700

    test: add first test to index page

--- api/settings.py
+++ api/settings.py
@@ -82,8 +82,16 @@ WSGI_APPLICATION = "api.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {}
-DATABASES["default"] = dj_database_url.config(conn_max_age=600)
+USE_POSTGRES = os.getenv('DATABASE_URL') is not None
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+if USE_POSTGRES:
+    DATABASES["default"] = dj_database_url.config(conn_max_age=600)
 
 # DRF settings
 REST_FRAMEWORK = {
