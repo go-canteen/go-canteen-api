7e5ef4d6820832e69c303169d23a87eff0db0915
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2022-09-27 20:35:17 +0700

    test: add unit test for canteen model

--- main/tests.py
+++ main/tests.py
@@ -1,7 +1,32 @@
-from django.test import  TestCase, tag
+from django.test import TestCase, tag
 from django.urls import reverse
+from main.models import Canteen
+
 
 class MainTestCase(TestCase):
     def test_root_url_status_200(self):
-        response = self.client.get('/')
+        response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
+
+
+class CanteenTestCase(TestCase):
+    def test_canteen_model(self):
+        canteen = Canteen.objects.create(
+            name="Canteen 1", address="Jl. Canteen 1", description="Canteen 1"
+        )
+        self.assertEqual(canteen.name, "Canteen 1")
+        self.assertEqual(canteen.address, "Jl. Canteen 1")
+        self.assertEqual(canteen.description, "Canteen 1")
+        self.assertEqual(canteen.image, None)
+        self.assertIsNotNone(canteen.date_joined)
+        self.assertIsNotNone(canteen.date_updated)
+        self.assertEqual(canteen.__str__(), "Canteen 1")
+
+    def test_canteen_serializer(self):
+        pass
+
+    def test_canteen_view(self):
+        pass
+
+    def test_canteen_url(self):
+        pass
