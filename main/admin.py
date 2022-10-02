25771c20daf3acc96755ef3fba6ab1d3ee1e408f
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2022-10-02 13:28:57 +0700

    [GREEN] feat: add model for canteen banner carousel

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from main.models import Canteen
+from main.models import Canteen, CanteenBanner
 
 admin.site.register(Canteen)
+admin.site.register(CanteenBanner)
--- main/models.py
+++ main/models.py
@@ -13,3 +13,16 @@ class Canteen(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class CanteenBanner(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    canteen = models.ForeignKey(Canteen, on_delete=models.CASCADE)
+    image = models.URLField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    on_click_url = models.URLField(max_length=255, blank=True, null=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.canteen.name + ": " + str(self.id)
