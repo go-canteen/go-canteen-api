5efb8cdeb5c33c3cdf74779e26f2584d5c6aba0b
mochammed.daffa91 <mochammed.daffa91@ui.ac.id>
2022-10-12 00:53:21 +0700

    [RED] Added class-based views for Canteen List & Canteen Detail

--- main/views.py
+++ main/views.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
+from rest_framework.response import Response
 from django.contrib.auth.models import User
 from rest_framework import viewsets
-from main.serializers import UserSerializer
+from main.models import Canteen
+from main.serializers import CanteenSerializer, UserSerializer
 from rest_framework import authentication, permissions
 from rest_framework.views import APIView
+from rest_framework import status
+from django.http import Http404
+
 import logging
 
 logger = logging.getLogger(__name__)
@@ -21,7 +26,41 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
 class CanteenList(APIView):
-    pass 
+
+    def get(self, request, format=None):
+        canteens = Canteen.objects.all()
+        serializer = CanteenSerializer(canteens, many=True)
+        return Response({"canteens": serializer.data})
+
+    def post(self, request, format=None):
+        serializer = CanteenSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class CanteenDetail(APIView):
-    pass
+    def get_object(self, pk):
+        try:
+            return Canteen.objects.get(pk=pk)
+        except Canteen.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        canteen = self.get_object(pk)
+        serializer = CanteenSerializer(canteen)
+        return Response(serializer.data)
+    
+    def put(self, request, pk, format=None):
+        canteen = self.get_object(pk)
+        serializer = CanteenSerializer(canteen, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
