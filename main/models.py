4bdb1eb9884cf28828276d4f85189b94da56da89
mochammed.daffa91 <mochammed.daffa91@ui.ac.id>
2022-11-23 13:21:01 +0700

    [FEAT] Added custom save function

--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 import uuid
-
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters.html import HtmlFormatter
+from pygments import highlight
 
 class Canteen(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -10,10 +12,24 @@ class Canteen(models.Model):
     image = models.URLField(max_length=255, blank=True, null=True)
     date_joined = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey('auth.User', related_name='canteens', on_delete=models.CASCADE)
+    highlighted = models.TextField()
 
     def __str__(self):
         return self.name
 
+    def save(self, *args, **kwargs):
+        """
+        Use the `pygments` library to create a highlighted HTML
+        representation of the code snippet.
+        """
+        lexer = get_lexer_by_name(self.language)
+        linenos = 'table' if self.linenos else False
+        options = {'title': self.title} if self.title else {}
+        formatter = HtmlFormatter(style=self.style, linenos=linenos,
+                              full=True, **options)
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super().save(*args, **kwargs)
 
 class CanteenBanner(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
