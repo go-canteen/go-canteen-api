c65097405bff9dfbae5114ee235f94a024d62f6f
Zackho Wisely Darmawan <wizackho@gmail.com>
2022-12-05 21:09:02 +0700

    [RED] add test for menu model

--- main/tests.py
+++ main/tests.py
@@ -1,6 +1,6 @@
 from django.test import  TestCase, tag
 from django.urls import reverse
-from main.models import Canteen
+from main.models import Canteen, Menu
 
 class MainTestCase(TestCase):
     def test_root_url_status_200(self):
@@ -12,6 +12,9 @@ class CanteenTestCase(TestCase):
         self.canteen = Canteen.objects.create(
             name="Canteen 1", address="Jl. Canteen 1", description="Canteen 1"
         )
+        self.menu = Menu.objects.create(
+            canteen = self.canteen, name="Menu 1", price=1000, kelompok="kelompok 1", description="Menu 1"
+        )
         return super().setUp()
 
 def test_canteen_model(self):
@@ -22,3 +25,12 @@ def test_canteen_model(self):
         self.assertIsNotNone(self.canteen.date_joined)
         self.assertIsNotNone(self.canteen.date_updated)
         self.assertEqual(self.canteen.__str__(), "Canteen 1")
+
+def test_menu_model(self):
+    self.assertEqual(self.menu.canteen, "Canteen 1")
+    self.assertEqual(self.menu.name, "Menu 1")
+    self.assertEqual(self.canteen.price, 1000)
+    self.assertEqual(self.canteen.description, "Canteen 1")
+    self.assertEqual(self.canteen.image, None)
+    self.assertIsNotNone(self.canteen.kelompok, "kelompok 1")
+    self.assertEqual(self.canteen.__str__(), "Menu 1")
