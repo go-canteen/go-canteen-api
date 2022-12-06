5b9cd68cc7cbe356bbca6125ac6ef755d4f933a8
unknown <kevin2wangsa@gmail.com>
2022-12-06 08:28:34 +0700

    [GREEN] Implentation Order Model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Order(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.CharField(max_length=255)
+    merchant  = models.CharField(max_length=255)
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+def __str__(self):
+    return self.name
